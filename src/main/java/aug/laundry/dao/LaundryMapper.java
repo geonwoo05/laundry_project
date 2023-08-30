6de6a591de864d247aed2857606ac9757df0700e
김승우 <tmd8633@naver.com>
2023-08-30 14:43:21 +0900

    device 추가

--- src/main/java/aug/laundry/dao/LaundryMapper.java
+++ src/main/java/aug/laundry/dao/LaundryMapper.java
@@ -2,7 +2,6 @@ package aug.laundry.dao;
 
 import aug.laundry.domain.CouponList;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
