674d1e98176f06998a9e520d2f163c2b696d5dd7
itjun2025 <cloudyjun@naver.com>
2023-09-01 17:29:09 +0900

    feat: html 수정

---
+++ src/main/java/aug/laundry/service/DrycleaningService_sdj.java
@@ -0,0 +1,10 @@
+package aug.laundry.service;
+
+import aug.laundry.dto.DrycleaningDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface DrycleaningService_sdj {
+
+    public int insert(DrycleaningDto dto);
+}
---
+++ src/main/java/aug/laundry/service/RepairService_sdj.java
@@ -0,0 +1,10 @@
+package aug.laundry.service;
+
+import aug.laundry.dto.RepairDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface RepairService_sdj {
+
+    public int insert(RepairDto dto);
+}
