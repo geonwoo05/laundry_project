0f6d63330adcf375e000215936d533f6427861cb
donghyunkim <ghffu405@gmail.com>
2023-09-04 16:42:48 +0900

    feat: 배송정책변경, 코드수정

--- src/main/java/aug/laundry/enums/category/CategoryOption.java
+++ src/main/java/aug/laundry/enums/category/CategoryOption.java
@@ -5,12 +5,12 @@ import lombok.Getter;
 @Getter
 public enum CategoryOption {
 
-    QUICK_LAUNDRY("빠른세탁"),
     COMMON_LAUNDRY("생활빨래"),
     DRYCLEANING("드라이클리닝"),
     REPAIR("수선");
 
     private String title;
+
 
     CategoryOption(String title) {
         this.title = title;
