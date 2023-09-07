84c6aaaed03c1b15fc955276b97d4c44c6ea73ab
sohee <sohee17802@gmail.com>
2023-09-07 17:54:50 +0900

    feat:구독결제

--- src/main/java/aug/laundry/dto/SubscriptionPayDto.java
+++ src/main/java/aug/laundry/dto/SubscriptionPayDto.java
@@ -9,15 +9,15 @@ import lombok.ToString;
 @ToString
 public class SubscriptionPayDto {
     private Long subscriptionPayId;
-    private Long memberId; // 로그인 아이디
-    private int selectMonth;// 구독 개월 수
-    private String merchantUid;
-    private String customerUid;
-    private String name;
+    private Long member_id; // 로그인 아이디
+    private int select_month;// 구독 개월 수
+    private String merchant_uid;
+    private String merchant_uid_r; // 다음번 예약 uid
+    private String customer_uid;
     private int amount;
-    private String payDate;
-    private char subscriptionStatus;
-    private String impUid;
+    private String pay_date;
+    private char subscription_status;
+    private String imp_uid;
 
     public SubscriptionPayDto() {
 
